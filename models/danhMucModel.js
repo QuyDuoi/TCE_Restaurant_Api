9f1c7231a71beb9a5fb3f4fa2a7598ab22fe22b2
QuyDuoi <quynsph41939@fpt.edu.vn>
2024-09-30 22:19:35 +0700

    FixDanhMucModel

--- models/danhMucModel.js
+++ models/danhMucModel.js
@@ -3,6 +3,11 @@ const { mongoose } = require("../config/db");
 const danhMucSchema = new mongoose.Schema(
   {
     tenDanhMuc: { type: String, required: true },
+    id_nhaHang: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "NhaHang",
+      required: true,
+    },
   },
   {
     collection: "DanhMuc",
