ebd9ef8985668373157094bf3978e2fa50d5cf82
Lab8 <lamndps33323@fpt.edu.vn>
2024-10-04 19:02:59 +0700

    update  hoadon

--- models/hoaDonModel.js
+++ models/hoaDonModel.js
@@ -27,6 +27,13 @@ const hoaDonSchema = new mongoose.Schema(
         required: false,
       },
     ],
+    id_caLamViec: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "CaLamViec",
+        required: false,
+      },
+    ]
   },
   {
     collection: "HoaDon",
