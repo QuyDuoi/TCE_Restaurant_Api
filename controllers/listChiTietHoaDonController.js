5185a2db191978c3ef334cbdd519c62735805b9d
NguyenTrien <trienntph41852@fpt.edu.vn>
2024-10-23 10:42:56 +0700

    update layChiTietHoaDon

--- controllers/listChiTietHoaDonController.js
+++ controllers/listChiTietHoaDonController.js
@@ -2,5 +2,5 @@ const { ChiTietHoaDon } = require("../models/chiTietHoaDonModel");
 const { HoaDon } = require("../models/hoaDonModel");
 
 exports.addListChiTietHoaDon = async (req, res, next) => {
-  
+   
 };
