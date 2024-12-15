365fc51c2b4462a119f259108c4b0603e6499fcc
QuyDuoi <quynsph41939@fpt.edu.vn>
2024-12-15 09:59:29 +0700

    Cấp quyền connect cho website và localhost

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ var apiRouter = require("./routes/api");
 var authRouter = require("./routes/auth"); // Thêm dòng này
 
 const corsOptions = {
-  origin: "https://tce-restaurant-webapp.onrender.com", // React chạy trên localhost
+  origin: function (origin, callback) {
+    if (origin === "http://localhost:3000" || origin === "https://tce-restaurant-webapp.onrender.com") {
+      callback(null, true);  // Cho phép các yêu cầu từ localhost và từ webapp
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Cho phép gửi cookie, nếu cần
 };
